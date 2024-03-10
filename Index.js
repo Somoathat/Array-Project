da4a767fba67725fa32becc47ff430d18eb9c2d8
Evan Graham <evangraham19@gmail.com>
2024-03-10 15:34:16 -0700

    array

---
+++ Index.js
@@ -0,0 +1,17 @@
+// array = a varible like structure that can hold more than one value
+
+let fruits = ["apple", "orange", "bananna"];
+
+//fruits[3] = "coconut";
+// fruits.push("coconut");
+// fruits.pop();
+fruits.unshift("mango");
+
+//console.log(fruits[0]);
+//console.log(fruits[1]);
+//console.log(fruits[2]);
+//console.log(fruits[3]);
+
+let numOFFruits = fruits.length;
+let index = fruits.indexOf("apple");
+console.log(numOFFruits);
